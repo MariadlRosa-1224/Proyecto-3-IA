855e4a23dd9a38bd6942a623f325ae5b465fe892
joalvarado27 <146464326+joalvarado27@users.noreply.github.com>
2025-05-11 02:21:48 -0500

    Fase 2

--- ReglaDifusa.java
+++ ReglaDifusa.java
@@ -1,3 +1,33 @@
+// Clase que representa una regla difusa en el sistema de inferencia
 public class ReglaDifusa {
     
+    // Atributos de la regla
+    private String variableEntrada1;
+    private String conjuntoDifuso1;
+    private String variableEntrada2;
+    private String conjuntoDifuso2;
+    private String operador;
+    private String variableSalida;
+    private String conjuntoSalida;
+
+    // Constructor
+    public ReglaDifusa(String variableEntrada1, String conjuntoDifuso1,
+                       String variableEntrada2, String conjuntoDifuso2,
+                       String operador, String variableSalida, String conjuntoSalida) {
+        this.variableEntrada1 = variableEntrada1;
+        this.conjuntoDifuso1 = conjuntoDifuso1;
+        this.variableEntrada2 = variableEntrada2;
+        this.conjuntoDifuso2 = conjuntoDifuso2;
+        this.operador = operador;
+        this.variableSalida = variableSalida;
+        this.conjuntoSalida = conjuntoSalida;
+    }
+
+    // Método para visualizar la regla en formato legible
+    @Override
+    public String toString() {
+        return "SI " + variableEntrada1 + " ES " + conjuntoDifuso1 + " " +
+               operador + " " + variableEntrada2 + " ES " + conjuntoDifuso2 + 
+               " ENTONCES " + variableSalida + " ES " + conjuntoSalida;
+    }
 }
